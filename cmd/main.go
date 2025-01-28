24576421a5db08391ce62c3531cb78c20d7dda41
tomiristapen <tomiristn@gmail.com>
2025-01-28 13:23:42 +0600

    added routes and middleware

--- cmd/main.go
+++ cmd/main.go
@@ -1,36 +1,81 @@
 package main
 
 import (
-	"ClothesShop/config"
-	"ClothesShop/internal/handlers"
-	"ClothesShop/internal/repository"
-	"ClothesShop/internal/services"
-	"ClothesShop/migrations"
-	"ClothesShop/routes"
-	"log"
+    "ClothesShop/config"
+    "ClothesShop/internal/handlers"
+    "ClothesShop/internal/repository"
+    "ClothesShop/internal/services"
+    "ClothesShop/middleware"
+    "ClothesShop/migrations"
+    "ClothesShop/routes"
+    "log"
+    "github.com/gin-gonic/gin"
 )
 
 func main() {
-	config.InitDB()
+    config.InitDB()
 
-	migrations.Migrate()
+    migrations.Migrate()
 
-	userRepo := &repository.UserRepository{
-		DB: config.DB,
-	}
+    userRepo := &repository.UserRepository{
+        DB: config.DB,
+    }
+    userService := &services.UserService{
+        Repo: userRepo,
+    }
+    userHandlers := &handlers.UserHandlers{
+        Service: userService,
+    }
 
-	userService := &services.UserService{
-		Repo: userRepo,
-	}
+    orderRepo := &repository.OrderRepository{
+        DB: config.DB,
+    }
+    orderService := &services.OrderService{
+        Repo: orderRepo,
+    }
+    orderHandlers := &handlers.OrderHandlers{
+        Service: orderService,
+    }
 
-	userHandlers := &handlers.UserHandlers{
-		Service: userService,
-	}
+    cartRepo := &repository.CartRepository{
+        DB: config.DB,
+    }
+    cartService := &services.CartService{
+        Repo: cartRepo,
+    }
+    cartHandlers := &handlers.CartHandlers{
+        Service: cartService,
+    }
 
-	router := routes.SetupRoutes(userHandlers)
 
-	log.Println("Сервер запущен на порту 8080...")
-	if err := router.Run(":8080"); err != nil {
-		log.Fatalf("Ошибка запуска сервера: %v", err)
-	}
+    productRepo := &repository.ProductRepository{
+        DB: config.DB,
+    }
+    productService := &services.ProductService{
+        Repo: productRepo,
+    }
+    productHandlers := &handlers.ProductHandlers{
+        Service: productService,
+    }
+
+ 
+    router := gin.Default()
+    router.Use(middleware.LoggingMiddleware()) 
+    router.Use(middleware.AuthMiddleware())    
+
+    routes.SetupUserRoutes(router, userHandlers)        
+    routes.SetupOrderRoutes(router, orderHandlers)       
+    routes.SetupCartRoutes(router, cartHandlers)         
+    routes.SetupProductRoutes(router, productHandlers)   
+
+    router.GET("/health-check", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "status": "OK",
+        })
+    })
+
+    log.Println("Server running on port 8080...")
+    if err := router.Run(":8080"); err != nil {
+        log.Fatalf("Failed to start server: %v", err)
+    }
 }
